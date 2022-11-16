4eb1631cf685b32c9e173c8170dd3dd742e732f6
xuandrew <xu.andrew@163.com>
2022-11-16 22:20:05 +0800

    Update cokemv.js

--- Script/Web/cokemv.js
+++ Script/Web/cokemv.js
@@ -1,3 +1,3 @@
 var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/cokemv.css" type="text/css">');
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/xuandrew/Quantumult-X/master/Script/Web/CSS/cokemv.css" type="text/css">');
 $done({ body });
