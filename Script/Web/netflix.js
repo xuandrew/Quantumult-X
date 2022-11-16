bf070dae38daa4646cbb6f98ba78b276efaf3c51
xuandrew <xu.andrew@163.com>
2022-11-16 22:24:15 +0800

    Update netflix.js

--- Script/Web/netflix.js
+++ Script/Web/netflix.js
@@ -1,5 +1,5 @@
 
 /* 公众号墨鱼手记 crated by ddgksf2013 on 2022-10-30 */
 var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/netflix.css" type="text/css">');
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/xuandrew/Quantumult-X/master/Script/Web/CSS/netflix.css" type="text/css">');
 $done({ body });
