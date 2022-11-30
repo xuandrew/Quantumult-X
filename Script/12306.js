41961f8f48c66e302597492644b50a133cdb41c3
xuandrew <xu.andrew@163.com>
2022-11-30 20:05:21 +0800

    Create 12306.js

---
+++ Script/12306.js
@@ -0,0 +1,1 @@
+var ddgksf2013=JSON.parse($response.body);ddgksf2013.materialsList&&(1==ddgksf2013.materialsList.length?(ddgksf2013.materialsList[0].filePath="",ddgksf2013.advertParam.skipTime=1,ddgksf2013.advertParam.skipTimeAgain=5,ddgksf2013.advertParam.showSkipBtn=-1):1<ddgksf2013.materialsList.length&&(ddgksf2013.materialsList=[{}])),$done({body:JSON.stringify(ddgksf2013)});
