3f5fe9feba08cd9ae7b3f1c3599431f63ef79936
xuandrew <xu.andrew@163.com>
2022-12-02 20:24:45 +0800

    Update bilibili_json.js

--- Script/bilibili_json.js
+++ Script/bilibili_json.js
@@ -26,7 +26,11 @@ if (magicJS.read(blackKey)) {
                             for (let banner of item["banner_item"]) {
                                 if (banner["type"] === "ad") {
                                     continue;
-                                } else if (banner["static_banner"] && banner["static_banner"]["is_ad_loc"] != true) {
+                                }
+                                else if (banner["type"] === "static") {
+                                    continue;
+                                }
+                                else if (banner["static_banner"] && banner["static_banner"]["is_ad_loc"] != true) {
                                     bannerItems.push(banner);
                                 }
                             }
@@ -137,19 +141,7 @@ if (magicJS.read(blackKey)) {
                         delete obj["data"]["sections_v2"][index].tip_icon;
                         delete obj["data"]["sections_v2"][index].tip_title;
                         //2022-02-16 add by ddgksf2013
-                        for (let ii = 0; ii < obj["data"]["sections_v2"].length; ii++) {
-                            if (obj.data.sections_v2[ii].title == "推荐服务" || obj.data.sections_v2[ii].title == "推薦服務") {
-                                //obj.data.sections_v2[ii].items[0].title='\u516C\u773E\u865F';
-                                //obj.data.sections_v2[ii].items[1].title='\u58A8\u9B5A\u624B\u8A18';
-                            }
-                            if (obj.data.sections_v2[ii].title == "更多服務" || obj.data.sections_v2[ii].title == "更多服务") {
-                                if (obj.data.sections_v2[ii].items[0].id == 500) {
-                                    //obj.data.sections_v2[ii].items[0].title='\u516C\u773E\u865F';
-                                }
-                                if (obj.data.sections_v2[ii].items[1].id == 501) {
-                                    //obj.data.sections_v2[ii].items[1].title='\u58A8\u9B5A\u624B\u8A18';
-                                }
-                            }
+                        for (let ii = 0; ii < obj["data"]["sections_v2"].length; ii++) {                                                        
                             if (obj.data.sections_v2[ii].title == "创作中心" || obj.data.sections_v2[ii].title == "創作中心") {
                                 delete obj.data.sections_v2[ii].title;
                                 delete obj.data.sections_v2[ii].type;
@@ -241,7 +233,7 @@ if (magicJS.read(blackKey)) {
                             module.items = module.items.filter((i) => i.blink.indexOf("www.bilibili.com") == -1);
                         }
                         if (module.style.startsWith("tip")) {
-                            module.items = null;
+                            module.items = [];
                         }
                     });
                     body = JSON.stringify(obj);
@@ -262,7 +254,7 @@ if (magicJS.read(blackKey)) {
                             module.items = module.items.filter((i) => i.blink.indexOf("www.bilibili.com") == -1);
                         }
                         if (module.style.startsWith("tip")) {
-                            module.items = null;
+                            module.items = [];
                         }
                     });
                     body = JSON.stringify(obj);
@@ -307,7 +299,7 @@ if (magicJS.read(blackKey)) {
             case /^https:\/\/app\.bilibili\.com\/x\/v2\/splash\/list/.test(magicJS.request.url):
                 try {
                     let obj = JSON.parse(magicJS.response.body);
-                    if (obj.data) {
+                    if (obj.data&&obj.data.list) {
                         for (let item of obj["data"]["list"]) {
                             item["duration"] = 0;
                             // 显示时间
