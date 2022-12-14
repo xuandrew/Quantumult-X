23503023edb6b5b01dd0c4dbb74d5c7acf1ead27
xuandrew <xu.andrew@163.com>
2022-12-14 20:50:09 +0800

    Update weibo.js

--- Script/weibo.js
+++ Script/weibo.js
@@ -293,7 +293,7 @@ function removePage(data){
 
 	// 删除热搜列表置顶条目
 	if (mainConfig.removePinedTrending && data.cards && data.cards.length > 0) {
-		if (data.cards[0].card_group) {
+		if (data.cards[0].card_group&&data.cards[0].card_group[0].itemid) {
 			data.cards[0].card_group = data.cards[0].card_group.filter(c=>!c.itemid.includes("t:51"));
 		}
 	}
