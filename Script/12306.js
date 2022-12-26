a47bda300229c8347a9f7c674d5eed3f23ad8acc
xuandrew <xu.andrew@163.com>
2022-12-26 16:38:39 +0800

    更新 12306.js

--- Script/12306.js
+++ Script/12306.js
@@ -1,1 +1,11 @@
-var obj=JSON.parse($response.body);obj.materialsList&&(1===obj.materialsList.length?(obj.materialsList[0].filePath="",obj.advertParam.skipTime=1,obj.advertParam.lg=1):obj.materialsList.length>1&&(obj.materialsList=[{}])),$done({body:JSON.stringify(obj)});
+var obj = JSON.parse($response.body);
+if (obj.materialsList) {
+    if (obj.materialsList.length === 1) {
+        obj.materialsList[0].filePath = "";
+        obj.advertParam.skipTime = 1;
+        obj.advertParam.lg = 1;
+    } else if (obj.materialsList.length > 1) {
+        obj.materialsList = [{}];
+    }
+}
+$done({body: JSON.stringify(obj)});
