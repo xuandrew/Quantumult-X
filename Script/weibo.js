a27e258f74968d560db6d644d0ab3b864ee3ceb1
xuandrew <xu.andrew@163.com>
2023-02-10 23:07:13 +0800

    更新 weibo.js

--- Script/weibo.js
+++ Script/weibo.js
@@ -10,32 +10,32 @@ const blockIds = [];
 
 // 微博详情页菜单配置
 const itemMenusConfig = {
-  creatortypeask: false, // 转发任务
+  creatortypeask: true, // 转发任务
   mblog_menus_apeal: true, // 申诉
-  mblog_menus_avatar_widget: false, // 用此头像挂件
+  mblog_menus_avatar_widget: true, // 用此头像挂件
   mblog_menus_bullet_shield: true, // 屏蔽弹幕
-  mblog_menus_card_bg: false, // 用此卡片背景
+  mblog_menus_card_bg: true, // 用此卡片背景
   mblog_menus_comment_manager: true, // 评论管理
   mblog_menus_copy_url: true, // 复制链接
-  mblog_menus_custom: false, // 寄微博
+  mblog_menus_custom: true, // 寄微博
   mblog_menus_delete: true, // 删除
   mblog_menus_edit: true, // 编辑
   mblog_menus_edit_history: true, // 编辑记录
   mblog_menus_edit_video: true, // 编辑视频
   mblog_menus_favorite: true, // 收藏
   mblog_menus_follow: true, // 关注
-  mblog_menus_home: false, // 返回首页
+  mblog_menus_home: true, // 返回首页
   mblog_menus_long_picture: true, // 生成长图
   mblog_menus_modify_visible: true, // 设置分享范围
-  mblog_menus_novelty: false, // 新鲜事投稿
-  mblog_menus_open_reward: false, // 赞赏
-  mblog_menus_popularize: false,
+  mblog_menus_novelty: true, // 新鲜事投稿
+  mblog_menus_open_reward: true, // 赞赏
+  mblog_menus_popularize: true,
   mblog_menus_promote: false, // 推广
   mblog_menus_report: true, // 投诉
   mblog_menus_shield: true, // 屏蔽
   mblog_menus_sticking: true, // 置顶
-  mblog_menus_video_feedback: false, // 播放反馈
-  mblog_menus_video_later: false // 可能是稍后再看
+  mblog_menus_video_feedback: true, // 播放反馈
+  mblog_menus_video_later: true // 可能是稍后再看
 };
 
 if (url.includes("/interface/sdk/sdkad.php")) {
