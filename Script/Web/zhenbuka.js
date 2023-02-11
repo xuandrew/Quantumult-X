94eddd7de9c38ccb8c0d7e9993318a4f85648e6e
xuandrew <xu.andrew@163.com>
2023-02-11 21:13:46 +0800

    更新 zhenbuka.js

--- Script/Web/zhenbuka.js
+++ Script/Web/zhenbuka.js
@@ -1,3 +1,3 @@
 /* 公众号墨鱼手记 crated by ddgksf2013 on 2022-05-28 */
-var body = $response.body.replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/xuandrew/Quantumult-X/master/Script/Web/CSS/zhenbuka.css" type="text/css">').replace(/'6572'/g, "'6578'").replace(/\<div  class=\"item stui-banner__item[\s\S]*html[\s\S]*?\<\/div\>/g, "");
+var body = $response.body.replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/xuandrew/Quantumult-X/master/Script/Web/CSS/zhenbuka.css" type="text/css">').replace(/'6572'/g, "'6578'").replace(/\<div  class=\"item stui-banner__item[\s\S]*html[\s\S]*?\<\/div\>/g, "").replace(/img id="ik\d+"/g, 'img id="ddgksf2013"');
 $done({ body });
