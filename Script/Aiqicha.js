34e6c5e4ff9b7dcebf2c5e5e7a35e5a7c39909e6
xuandrew <xu.andrew@163.com>
2023-02-17 12:38:41 +0800

    Update Aiqicha.js

--- Script/Aiqicha.js
+++ Script/Aiqicha.js
@@ -1,3 +1,15 @@
+/*
+爱企查vip
+^https:\//aiqicha.baidu.com\/usercenter\/getvipinfoajax url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/aiqicha.js
+mitm= aiqicha.baidu.com
+*/
+
+/*var body = $response.body
+    .replace(/\"svip\":\{\"status\":0,\"startTime\":\"\",\"endTime\":\"\"/, "\"svip\":\{\"status\":1,\"startTime\":\"\",\"endTime\":\"2099-12-31\"");
+$done({ body });
+*/
+
+
 let obj = JSON.parse($response.body);
 obj.data = {
 "vip": 1,
