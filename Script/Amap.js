557228abf9ba3064acfe898d6560826fe04ebdf2
xuandrew <xu.andrew@163.com>
2023-02-17 14:35:24 +0800

    更新 Amap.js

--- Script/Amap.js
+++ Script/Amap.js
@@ -79,7 +79,7 @@ if (obj.data) {
       // "packageShelf",
       // "smallOrListBizRec",
       "smallListBizRec", // 周边热门酒店
-      "multi_page_anchor", // 二级导航菜单 门票 评论 推荐
+      // "multi_page_anchor", // 二级导航菜单 门票 评论 推荐
       "bigListBizRec", // 周边景点推荐 三张景点大图
       "nearbyRecommendModule", // 周边推荐
       "scenic_recommend", // 景点建议
