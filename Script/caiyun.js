4d19bfa5582776eec18bbd88dacebb9599b641d6
xuandrew <xu.andrew@163.com>
2023-05-12 23:28:32 +0800

    Update caiyun.js

--- Script/caiyun.js
+++ Script/caiyun.js
@@ -97,7 +97,7 @@ chxm1023.result.vip_type = "s";
 chxm1023.result.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps";
 chxm1023.result.bound_status.qq = {
         "id" : "63592fa7e7a295001888256b",
-        "username" : "chxm1023",
+        "username" : "xuandrew",
         "is_bound" : true
       };
 chxm1023.result.bound_status.weixin = {
