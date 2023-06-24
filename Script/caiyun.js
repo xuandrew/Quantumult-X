74c37f11c984f49e30121cc0aaa97784ca52f25e
xuandrew <xu.andrew@163.com>
2023-06-24 17:19:49 +0800

    更新 caiyun.js

--- Script/caiyun.js
+++ Script/caiyun.js
@@ -4,34 +4,21 @@ const yza = '/visitors';
 ;const yh = '/user_info';
 
 if ($request.url.indexOf(user) != -1){
-;chxm1023.result.device_id = "D1F2A3F9-093E-4775-9E49-80DEEE9CC1C3";
 chxm1023.result.device_id = "78B61F3B-706F-44E8-9E4D-F68BDA1BA896";
-;chxm1023.result._id = "6432c86484ea830012de2019";
 chxm1023.result._id = "6358cb93e7a295001482d9aa";
-chxm1023.result.is_visitor = false;
 chxm1023.result.svip_given = 365;
 chxm1023.result.ranking_above = 91;
-;chxm1023.result.platform_name = "";
+chxm1023.result.is_visitor = false;
 chxm1023.result.is_phone_verified = true;
-chxm1023.result.last_acted_at = 1687582646.1454451084
 chxm1023.result.hasBeenInvited = true;
 chxm1023.result.is_xy_vip = true;
 chxm1023.result.vip_expired_at = 4092599349;
 chxm1023.result.is_vip = true;
 chxm1023.result.xy_svip_expire = 4092599349;
-chxm1023.result.auto_renewal_type = "";
-;chxm1023.result.third_party_id = null;
-;chxm1023.result.third_party_ids = [
-;     "64679dcd4050ad001760d58c",
-;     "64679de55e4f320018d37a04",
-;     "64679dec41787e0013c32cd3"
-;    ];
 chxm1023.result.third_party_ids = [
-    "63592fa7e7a295001888256b",
-    "639ac02db1839300133031c0"
+     "63592fa7e7a295001888256b",
+      "639ac02db1839300133031c0"
     ];
-chxm1023.result.name = "xuandrew";
-;chxm1023.result.free_trial = 0;
 chxm1023.result.wt.vip = {
         "is_auto_renewal" : false,
         "enabled" : true,
@@ -42,31 +29,31 @@ chxm1023.result.wt.vip = {
       };
 chxm1023.result.wt.svip_given = 365;
 chxm1023.result.wt.ranking_above = 91;
-chxm1023.result.avatar = "https://tvax4.sinaimg.cn/crop.0.0.512.512.180/5b346ee9ly8h2rbudgjzvj20e80e8wet.jpg?KID=imgbed,tva&Expires=1687458878&ssig=8y8aNZ7Vzz";
+chxm1023.result.name = "xuandrew";
+chxm1023.result.avatar = "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLia6zPUuGQKVOJk2gnjpjHxSIuH7XaOJF2gEk9ic35ibib4QzUcbvNu6EpdHDc1Vciat1xg63ibK6EptWw/132";
 chxm1023.result.phone_num = "13145200000";
 chxm1023.result.vip_take_effect = 1;
 chxm1023.result.is_auto_renewal = false;
 chxm1023.result.is_primary = true;
 chxm1023.result.xy_vip_expire = 0;
-;chxm1023.result.platform_id = "";
 chxm1023.result.platform_id = "o3rJ_t00r0mxqS6GCVWMaVtEFLUk";
 chxm1023.result.svip_expired_at = 4092599349;
 chxm1023.result.svip_take_effect = 1;
 chxm1023.result.vip_type = "s";
 chxm1023.result.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps";
 chxm1023.result.bound_status.qq = {
-        "id" : "64679de55e4f320018d37a04",
-        "username" : "Hsu",
+        "id" : "63592fa7e7a295001888256b",
+        "username" : "xuandrew",
         "is_bound" : true
       };
 chxm1023.result.bound_status.weixin = {
-        "id" : "64679dcd4050ad001760d58c",
-        "username" : "Hsu",
+        "id" : "639ac02db1839300133031c0",
+        "username" : "xuandrew",
         "is_bound" : true
       };
 chxm1023.result.bound_statuscaiyun = {
-        "id" : "6432c86484ea830012de2019",
-        "username" : "xuandrew",
+        "id" : "6358cb93e7a295001482d9aa",
+        "username" : "",
         "is_bound" : true
       };
 }
