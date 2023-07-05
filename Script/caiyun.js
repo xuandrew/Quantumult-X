3f4f37583e7449b52215de6391664517d9ef5af4
xuandrew <xu.andrew@163.com>
2023-07-05 09:13:31 +0800

    更新 caiyun.js

--- Script/caiyun.js
+++ Script/caiyun.js
@@ -136,3 +136,47 @@ chxm1023.gender = 1
 }
 
 $done({body : JSON.stringify(chxm1023)});
+
+
+/******************
+/**********************************************
+脚本功能：彩云天气 解锁会员
+使用声明：⚠️仅供学习交流，🈲️商业用途
+
+
+https://biz.cyapi.cn/v2/user?app_name=weather
+https://biz.cyapi.cn/v1/visitors
+https://biz.cyapi.cn/v3/login_by_code 登录
+
+
+[rewrite_local]
+^https:\/\/biz\.cyapi\.cn\/v\d\/(user\?app_name=weather|visitors|login_by_code)$ url script-response-body https://raw.githubusercontent.com/jizhi0520/QX/main/caiyun.js
+^https:\/\/biz\.cyapi\.cn\/p\/v1\/user_info url script-response-body https://raw.githubusercontent.com/jizhi0520/QX/main/caiyun-user.js
+
+
+[MITM]
+hostname = biz.cyapi.cn
+**********************************************/
+
+
+var url = $request.url;
+var obj = JSON.parse($response.body);
+let Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps";
+
+if (url.indexOf('/user') != -1 || url.indexOf('/visitors') != -1) {
+		obj.result.token = Token;
+	body = JSON.stringify(obj);
+}
+
+if (url.indexOf('/login_by_code') != -1) {
+	let obj = {
+		"status": "ok",
+		"result": {
+			"is_phone_verified": true,
+			"token": Token
+		},
+		"rc": 0
+	}
+	body = JSON.stringify(obj);
+}
+$done({body});
