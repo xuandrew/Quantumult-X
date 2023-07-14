988681a256f4a99253b03b626e3653564a58f13c
xuandrew <xu.andrew@163.com>
2023-07-14 21:27:51 +0800

    更新 caiyun-user.js

--- Script/caiyun-user.js
+++ Script/caiyun-user.js
@@ -23,12 +23,16 @@ let obj = JSON.parse($response.body);
       "completed_percent" : 100,
       "avatar" : "https://tvax4.sinaimg.cn/crop.0.0.512.512.180/5b346ee9ly8h2rbudgjzvj20e80e8wet.jpg?KID=imgbed,tva&Expires=1687458878&ssig=8y8aNZ7Vzz",
       "reg_days" : 0,
-      "birthday" : " ",
-      "city" : " ",
+      "birthday" : "1987-07-18",
+      "city" : "泰州市",
       "interests" : [
-        " "
+        "穿衣指南",
+        "空气质量",
+        "徒步",
+        "气象景观",
+        "气象现象"
       ],
       "name" : "xuandrew",
-      "gender" : 0
+      "gender" : 1
 }
 $done({body : JSON.stringify(obj)});
