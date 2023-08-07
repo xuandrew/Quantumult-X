fb3980a2f7155e73843c90191fd866d7917e7db2
xuandrew <xu.andrew@163.com>
2023-08-07 17:49:47 +0800

    更新 Amap.js

--- Script/Amap.js
+++ Script/Amap.js
@@ -1,4 +1,4 @@
-// 2023-08-06 11:55
+// 2023-08-07 16:15
 
 const url = $request.url;
 if (!$response.body) $done({});
@@ -376,8 +376,9 @@ if (url.includes("/faas/amap-navigation/main-page")) {
       for (let item of obj.tip_list) {
         if (
           ["12"].includes(item?.tip?.datatype_spec) ||
-          ["toplist"].includes(item?.tip?.result_type) ||
+          ["ad", "poi_ad", "toplist"].includes(item?.tip?.result_type) ||
           [
+            "ad",
             "exct_query_sug_merge_theme",
             "query_sug_merge_theme",
             "sp"
@@ -398,6 +399,8 @@ if (url.includes("/faas/amap-navigation/main-page")) {
         if (item?.tip_list?.length > 0) {
           for (let ii of item.tip_list) {
             if (["12"].includes(ii?.tip?.datatype_spec)) {
+              continue;
+            } else if (["ad", "poi_ad"].includes(ii?.tip?.result_type)) {
               continue;
             } else {
               newTip.push(ii);
